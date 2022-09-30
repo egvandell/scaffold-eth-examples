a9ec81005fadad6218b885b1526230ee925bb131
GREG VVAN DELL <gvandell@engineation.com>
2022-09-30 07:05:27 -0400

    initial fix of the no network issue

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,7 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "62e4d4d8e861447686db464b423bc591";
+//export const INFURA_ID = "62e4d4d8e861447686db464b423bc591";
+export const INFURA_ID = "5368681ff90642f3b937d497e924650f";
+
 //dev - 568af3da01a043ecb2ac80bf69927b38
 // prod - 62e4d4d8e861447686db464b423bc591
 //MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
